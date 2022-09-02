040c5d31ad76b0b2959a4f6aea34635c9644ec04
Lukas-Johansson <lukas.johansson4@elev.ga.ntig.se>
2022-09-02 13:32:47 +0200

    Mer saker

--- ssc/kryptering2.java
+++ ssc/kryptering2.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class kryptering2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Skriv något du vill göra haha roligt på? ");
-        char m = 'B';
+        System.out.println("Skriv det du vill få haha roligt på");
+        String word = input.nextLine();
+        char l = (char) word.length();
+        char o=word.charAt(0);
+        char m = o;
         char k = '(';
         char c = (char)(m^k);
         System.out.println(c);
-        System.out.print(c^k);
+        System.out.print((char) (c^k));
+        System.out.print(l);
     }
 }
--- ssc/test.java
+++ ssc/test.java
@@ -3,9 +3,27 @@ import java.util.List;
 import java.util.Random;
 
 public class test {
-    public static void main(String[] args) {
-        List<Integer> givenList = Arrays.asList(5, 8, 10);
-        Random rand = new Random();
-        int siffra = givenList.get(rand.nextInt(givenList.size()));
-    }
+        public static String makeStringsEqual(String str, String keyword) {
+            String ret = "";
+            int ci     = 0;
+
+            if (str.length() > keyword.length()) {
+                for(int i = 0; i < str.length(); i++) {
+                    if (str.charAt(i) != ' ') {
+                        if (ci == keyword.length()) {
+                            ci = 0;
+                        }
+                        ret += keyword.charAt(ci);
+                        ci++;
+                    } else {
+                        ret += " ";
+                    }
+                }
+            }
+
+            return ret;
+        }
+        public static void main(String[] args) {
+            System.out.println(makeStringsEqual("hello my", "test"));
+        }
 }
