c651cb9b7be17e886e2f17146fd89686b14df4c0
Lukas-Johansson <lukas.johansson4@elev.ga.ntig.se>
2022-09-23 13:39:43 +0200

    Mer saker

--- ssc/Crypt.java
+++ ssc/Crypt.java
@@ -3,44 +3,64 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Crypt {
-    String filename;
-    String message;
-    String keyFilename;
-    String key;
+    String Filename;
+    String Message;
+    String KeyFilename;
+    String Key;
 
     //Läsa in fil
     public String readFile(String filename)
     {
-        String msg = "";
+        String Message = "";
         try {
-            BufferedReader br = new BufferedReader(new FileReader(filename));
-            msg = br.readLine();
+            BufferedReader in = new BufferedReader(new FileReader(filename));
+            String str;
+            while ((str = in.readLine()) != null) {
+                Message += str;
+            }
+            in.close();
         } catch (IOException e) {
-            e.printStackTrace();
         }
-        return msg;
+        return Message;
     }
 
     public void crypt(String message, String Key) {
-        if(key == null)
-        {
-            key = readKey(filename);
+        String CryptMessage = "";
+        int i = 0;
+        for (char c : message.toCharArray()) {
+            int cInt = (int) c;
+            int keyInt = (int) Key.charAt(i);
+            int cryptInt = cInt ^ keyInt;
+            char cryptChar = (char) cryptInt;
+            CryptMessage += cryptChar;
+            i++;
+            if (i == Key.length()) {
+                i = 0;
+            }
         }
-        encrypt(message, key);
+        System.out.println(CryptMessage);
     }
 
 
     public String readKey(String KeyFilename) {
         String key = "";
         try {
-            BufferedReader br = new BufferedReader(new FileReader(keyFilename));
-            key = br.readLine();
+            BufferedReader in = new BufferedReader(new FileReader(KeyFilename));
+            String str;
+            while ((str = in.readLine()) != null) {
+                key += str;
+            }
+            in.close();
         } catch (IOException e) {
-         e.printStackTrace();
         }
         return key;
     }
 
-    private void encrypt(String message, String key) {
+    public static void main(String[] args) {
+        Crypt crypt = new Crypt();
+        String message = crypt.readFile("message.txt");
+        String key = crypt.readKey("key.txt");
+        crypt.crypt(message, key);
     }
 }
+
